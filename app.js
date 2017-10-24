a2ff7651682871013f75bbc54179b929904ca220
jamesrsilvester <jamesrsilvester@gmail.com>
2017-10-24 16:07:03 -0700

    added logic for the four possible search conditions

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// TODO: Example to Remove "https://api.tumblr.com/v2/blog/jamesrsilvester.tumblr.com/posts?api_key=qvPkgVRPIjA1uvzj0neyThBYknIaH1R3lqsT2e5xBdfrkxDrvz";
+
 const tumblrAPI = {
   baseUrl: 'https://api.tumblr.com/v2',
   blogEndpoint: '/blog/',
@@ -6,33 +8,34 @@ const tumblrAPI = {
   // TODO: Store API KEY AS ENV Variable?
   apiKey: 'qvPkgVRPIjA1uvzj0neyThBYknIaH1R3lqsT2e5xBdfrkxDrvz'
 }
-// TODO: Example to Remove "https://api.tumblr.com/v2/blog/jamesrsilvester.tumblr.com/posts?api_key=qvPkgVRPIjA1uvzj0neyThBYknIaH1R3lqsT2e5xBdfrkxDrvz";
 
 $(document).on("ready", function() {
   console.log("scripts linked");
 
   $("form").on("submit", function(e) {
     e.preventDefault();
-    handleSubmit();
-    // getAndRenderGifs();
+    //clear previous results from page
+    $(".post").remove();
+    //check for completed fields.
+    validateFormInput();
   });
 });
 
-function handleSubmit() {
-  getAndRenderPosts();
-  // TODO: Check what values have been submitted.
-  // TODO: Change URL PATH accordingly
-};
-
-function getAndRenderPosts() {
-  // TODO: Is this an appropriate place to store this?
-  var blogQuery = document.getElementById('blogInput').value;
-  var blogPath = `${blogQuery}.tumblr.com/posts;`
-  console.log(`Searching for ${blogQuery}.tumblr.com`);
-  if (!blogQuery) {
+function validateFormInput() {
+  //save both search terms as variables
+  var blogQuery = document.getElementById('blogInput').value.trim()
+  var tagQuery = document.getElementById('tagInput').value.trim();
+  var blogPath = `${blogQuery}.tumblr.com/posts;`;
+  // Condition 1 - Check whether both searches are empty.
+  if(blogQuery.length === 0 && tagQuery.length === 0){
     alert("Search must not be empty");
   }
-  else {
+
+  //Condition 2 - Only blog is being searched for.
+  if (blogQuery.length > 0 && tagQuery.length === 0) {
+    console.log("only blog has been submitted");
+    //AJAX blog search
+    console.log(`Searching for ${blogQuery}.tumblr.com`);
     $.ajax({
       method: "GET",
       url: tumblrAPI.baseUrl + tumblrAPI.blogEndpoint + blogPath,
@@ -43,17 +46,29 @@ function getAndRenderPosts() {
       error: onError
     })
   }
+
+  //Condition 3 - Both blog and tag were searched
+  if (blogQuery.length > 0 && tagQuery.length > 0){
+    console.log("Both have been submitted")
+  }
+
+  //Condition 4 - Only Tag was searched
+  if (blogQuery.length === 0 && tagQuery.length > 0){
+    console.log("Only tag submitted")
+  }
 }
 
+//AJAX SUCCESS RESPONSE
 function onSuccess(json) {
+  //save incoming JSON as variable
   var allPosts = json.response.posts;
   console.log("success: " + json.response.posts);
-  $(".post").remove();
   allPosts.forEach(function(link, index) {
     $("#results").append($("<li class='post'>" + link.short_url + "</li>"))
   })
 };
 
+//AJAX ERROR RESPONSE
 function onError(request, status, error) {
   console.log("error " + request.responseText);
 };
